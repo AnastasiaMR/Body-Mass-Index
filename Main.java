8bcaae2135e7b9ac2f22173820003ecc4e41d3c5
Anastasia Matryashina <matr-nastya@yandex.ru>
2021-02-25 00:04:11 +0300

    Изменение кода

--- Main.java
+++ Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        double bmi = service.calculate(1.75, 50);
-        System.out.printf("Ваш индекс массы тела " + "%.2f", bmi, "");
 
+        double bmi = service.calculate(1.75, 50);
+        System.out.printf("Ваш индекс массы тела %.2f\n", bmi);
+
+        double bmi2 = service.calculate(1.55, 35);
+        System.out.printf("Ваш индекс массы тела %.2f\n", bmi2);
+
+        double bmi3 = service.calculate(1.64, 86);
+        System.out.printf("Ваш индекс массы тела %.2f\n", bmi3);
     }
 }
